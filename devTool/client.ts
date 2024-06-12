2e385e96d4e81f186fa0cff595fdb0cfa52dae88
wangzhiwei52 <wangzhiwei52@meituan.com>
2024-06-12 19:51:42 +0800

    feat: add auto open browser

--- devTool/client.ts
+++ devTool/client.ts
@@ -2,6 +2,7 @@ import { WebSocket } from "ws";
 export const devtoolClientServer = () => {
   if (window.hp) return;
   window.hp = new WebSocket("ws://127.0.0.1:3000");
+  const hp = window.hp;
   hp.onopen = function () {
     const payload = {
       eventName: "connect",
@@ -10,7 +11,7 @@ export const devtoolClientServer = () => {
     // 以下通信章节讲到
     window.JSBridge.postMessage(payload);
   };
-  wx.onmessage = function (message) {
+  hp.onmessage = function (message) {
     console.log(message);
     // 以下通信章节讲到
     window._handleNativeEvent && window._handleNativeEvent(message.data);
--- devTool/init.ts
+++ devTool/init.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { socketChannelDriver } from "./socket-driver";
+import { exec } from "child_process";
 const AppInit = (port: number) => {
   return new Promise(() => {
     const app = express();
     app.use(express.static("pubilc"));
     app.listen(port, async () => {
-      console.log(`http://127.0.0.1:${port}`);
+      const url = `http://127.0.0.1:${port}`;
+      console.log(`Server running in ${url}`);
+      exec(`open ${url}`);
       await socketChannelDriver();
     });
   });
